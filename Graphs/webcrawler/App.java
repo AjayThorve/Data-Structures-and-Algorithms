29d9e301b3c3436128e539295b78ba09fc4bbcc1
Ajay Thorve <ajaythorve1994@gmail.com>
2017-10-30 10:05:28 -0400

    Added inputting url funcitonality

--- Graphs/webcrawler/App.java
+++ Graphs/webcrawler/App.java
@@ -1,9 +1,13 @@
 package webcrawler;
+import java.util.*;
 
 public class App{
     public static void main(String args[]){
         WebCrawler crawl = new WebCrawler();
-        String rootUrl = "https://ajaythorve.co.in/";
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter url to crawl for links: ");
+        
+        String rootUrl = sc.nextLine();
         crawl.discoverWeb(rootUrl);
     }
 }
